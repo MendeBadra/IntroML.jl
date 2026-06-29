1db2cf23f5b3e2638093cb16846897f6df9844d2
MendeBadra <mendamar345@gmail.com>
2026-06-29 12:55:43 +0200

    Deploy updated build

--- previews/PR3/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IntroML","category":"page"},{"location":"#Introduction-to-Machine-Learning","page":"Home","title":"Introduction to Machine Learning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Materials for the Introduction to Machine Learning workshop with the Machine Learning ⇌ Science Colaboratory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Supervised Learning: One step at a time","category":"page"}]
-}
---
+++ search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = IntroML","category":"page"},{"location":"index.html#Introduction-to-Machine-Learning","page":"Home","title":"Introduction to Machine Learning","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Materials for the Introduction to Machine Learning workshop with the Machine Learning ⇌ Science Colaboratory.","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Sin func interpolation","category":"page"}]
+}
